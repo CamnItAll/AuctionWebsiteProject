010f3f64c459340258ebdfce27db0f3ea59abd4c
CameronC <cameron33888@gmail.com>
2025-10-24 15:21:53 -0400

    Update AuctionApplication.java

--- auction/src/main/java/com/edu/yu/itec4020/auction/AuctionApplication.java
+++ auction/src/main/java/com/edu/yu/itec4020/auction/AuctionApplication.java
@@ -2,16 +2,9 @@ package com.edu.yu.itec4020.auction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AuctionApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(AuctionApplication.class);
-	}
+public class AuctionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuctionApplication.class, args);
