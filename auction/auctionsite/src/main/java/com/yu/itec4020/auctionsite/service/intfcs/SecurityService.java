7067d29162d9befb1576d0c4576cd46c6d218129
CameronC <cameron33888@gmail.com>
2025-10-25 16:23:30 -0400

    Delete auction/auctionsite directory

--- auction/auctionsite/src/main/java/com/yu/itec4020/auctionsite/service/intfcs/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package com.yu.itec4020.auctionsite.service.intfcs;
-
-public interface SecurityService {
-    String findLoggedInUsername();
-
-    void autoLogin(String username, String password);
-}
--- auction/auctionsite/src/main/java/com/yu/itec4020/auctionsite/service/intfcs/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.yu.itec4020.auctionsite.service.intfcs;
-
-import com.yu.itec4020.auctionsite.model.User;
-
-public interface UserService {
-    void save(User user);
-
-    User findByUsername(String username);
-}
